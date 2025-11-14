6bd0c82184bb9c8efcc8229ab4b2fa84be88ef51
Lucky Abdillah <luckyabdillah00@gmail.com>
2025-11-14 14:52:19 +0700

    chore: implement page icon

--- src/components/commons/PageHead/PageHead.tsx
+++ src/components/commons/PageHead/PageHead.tsx
@@ -11,7 +11,7 @@ const PageHead = (props: PropTypes) => {
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>{ title }</title>
-            <link rel="icon" href="/images/general/logo.svg" type="image/x-icon" />
+            <link rel="icon" href="/images/general/icon.svg" type="image/x-icon" />
         </Head>
     )
 }
